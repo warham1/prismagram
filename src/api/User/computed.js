8ef16e33819fc62d5f1833e63568769c40475ad3
warham1 <yellow9697@hanyang.ac.kr>
2019-08-21 04:45:10 +0900

    computed query

---
+++ src/api/User/computed.js
@@ -0,0 +1,44 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, __, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: parent.id }, { followers_some: { id: user.id } }]
+        });
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+
+      return;
+    },
+    isSelf: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parent.id;
+    }
+  },
+  Post: {
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          { 
+              user: { id: user.id } 
+          },
+          {
+            post: { id }
+          }
+        ]
+      });
+    }
+  }
+};
