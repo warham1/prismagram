fc38989294191e5ba0664e57d7163976c1ae544a
warham1 <yellow9697@hanyang.ac.kr>
2019-09-07 04:02:28 +0900

    최적화

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,15 +3,21 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { username, email, firstName= "", lastName= "", bio= "" } = args;
-      const user = await prisma.createUser({
+      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({
+        OR: [{ username }, { email }]
+      });
+      if (exists) {
+        throw Error("해당 사용자 이름 / 이메일 은 이미 존재합니다.");
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
